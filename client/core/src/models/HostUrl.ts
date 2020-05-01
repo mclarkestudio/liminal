22cfc6eae66473770ed9f3261cb38377fba45e0d
mclarke <mclarkestudio@gmail.com>
2020-05-01 13:54:37 -0400

    mobile LAN devlopment added, matts local IP

--- client/core/src/models/HostUrl.ts
+++ client/core/src/models/HostUrl.ts
@@ -3,7 +3,7 @@
 
 // You should use explicit ip address such as http://172.21.55.1 instead of http://localhost here if you would like to debug/test the app in LAN for mobile devices.
 // Android emulator also cannot fetch http://localhost correctly. see https://github.com/facebook/react-native/issues/10404 for the detail of this issue.
-export const HOST_NAME_DEV: string = "http://localhost";
+export const HOST_NAME_DEV: string = "http://192.168.50.232"; // Use: 'http://localhost' when not LAN hosting 
 export const HOST_NAME_ANDROID_LOCAL: string = "http://10.0.2.2"; // for Android emulator debug only
 export const HOST_PORT_DEV: number = 3000;
 // If you would like to change SERVER_PORT_DEV, please modify client/core/src/setupProxy.js accordingly.
@@ -25,5 +25,6 @@ export const HOST_URL_PROD = url;
 export const CORS_WHITELIST: string [] = [
     // Add the origins you would like to whitelist
     `${HOST_NAME_ANDROID_LOCAL}:${HOST_PORT_DEV}`,
+    `${HOST_NAME_DEV}:${HOST_PORT_DEV}`,
     HOST_URL_DEV
 ];
